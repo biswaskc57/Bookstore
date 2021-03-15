e6741681e2444e78b0208b69e99abac0716d482d
Bisss <bisss@Bissss-MacBook-Pro.local>
2021-03-15 17:03:39 +0200

    Some changes made in database

--- src/main/java/com/example/Bookstore/model/User.java
+++ src/main/java/com/example/Bookstore/model/User.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="usertable")
+@Table(name = "username", schema = "public", catalog = "postgres")
 public class User {
 
     @Id
