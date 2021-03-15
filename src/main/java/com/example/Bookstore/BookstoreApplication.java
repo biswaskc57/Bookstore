b6f6062b1dabd894d946880406026e23771fcad6
Bisss <bisss@Bissss-MacBook-Pro.local>
2021-03-15 14:07:07 +0200

    first commit

---
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -0,0 +1,51 @@
+package com.example.Bookstore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.model.Book;
+import com.example.Bookstore.model.BookRepository;
+import com.example.Bookstore.model.Category;
+import com.example.Bookstore.model.CategoryRepository;
+import com.example.Bookstore.model.User;
+import com.example.Bookstore.model.UserRepository;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
+		return (args) -> {
+
+			log.info("list of categories");
+			crepository.save(new Category("IT"));
+			crepository.save(new Category("Business"));
+			crepository.save(new Category("law"));
+			crepository.save(new Category("Marketing"));
+			crepository.save(new Category("Languages"));
+			
+			//Create users: admin/ admin user/user
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+			
+
+			log.info("fetch all books");
+			for (Book book : brepository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+
+	}
+}
---
+++ src/main/java/com/example/Bookstore/model/Book.java
@@ -0,0 +1,108 @@
+package com.example.Bookstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String title;
+	private String author;
+	private int year;
+	private int isbn;
+	private int price;
+	@ManyToOne
+	@JoinColumn(name = "categoryid")
+	private Category category;
+
+	public Book() {
+	}
+
+	public Book(String title, String author, int year, int isbn, int price, Category category) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+		this.category = category;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+
+		if (this.category != null) {
+			return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + +year + ", isbn=" + isbn
+					+ ", price=" + price + " , category:  " + this.getCategory() + "]";
+		} else {
+			return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + +year + ", isbn=" + isbn
+					+ ", price=" + price + "]";
+		}
+
+	}
+
+}
