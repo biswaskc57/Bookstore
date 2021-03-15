fbd4ef649bcf2d4f59d513e0db82cab16458236b
Bisss <bisss@Bissss-MacBook-Pro.local>
2021-03-15 20:24:08 +0200

    user credentials fixed

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -35,8 +35,8 @@ public class BookstoreApplication {
 			crepository.save(new Category("Languages"));
 			
 			//Create users: admin/ admin user/user
-			User user1 = new User("user123", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER123");
-			User user2 = new User("admin123", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN123");
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
 			urepository.save(user1);
 			urepository.save(user2);
 			
